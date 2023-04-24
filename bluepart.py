c3f2a7e5c0569f64276d1e6e726029fe7423158d
mlgarrett <michael.garrett@nau.edu>
2023-04-24 09:31:32 -0700

    - set up the bluepart module to drive a frontend with flask via driver - made a html page to handle the frontend interface

--- bluepart.py
+++ bluepart.py
@@ -1,96 +1,84 @@
 import cv2
 import json
 import zlib
-import base64
 import numpy
+import base64
 
-from rich.console import Console
+def scale_image(source, scale_percent):
+	# scale the image
+	# scale_percent = 0.10 # percent of original size
+	width = int(source.shape[1] * scale_percent)
+	height = int(source.shape[0] * scale_percent)
+	dim = (width, height)
+	  
+	# resize image
+	resized = cv2.resize(source, dim, interpolation = cv2.INTER_AREA)
 
-console = Console()
+	return resized
 
-# define a "palette" which associates color classifications with item ids
-palette = {0: "stone-path", 1: "concrete", 2: "refined-concrete", 3: "hazard-concrete-right", 4: "refined-hazard-concrete-left"}
+def construct_blueprint(resized_image, palette):
+	# reshape image
+	Z = resized_image.reshape((-1,3))
 
-# input an image (probably as ndarray)
-og_image = cv2.imread('boker_crop.jpg')
+	# convert to numpy.float32
+	Z = numpy.float32(Z)
 
-# scale the image
-scale_percent = 10 # percent of original size
-width = int(og_image.shape[1] * scale_percent / 100)
-height = int(og_image.shape[0] * scale_percent / 100)
-dim = (width, height)
-  
-# resize image
-resized = cv2.resize(og_image, dim, interpolation = cv2.INTER_AREA)
+	# define how many colors (clusters) to use
+	K = len(palette)
 
-# reshape image
-Z = resized.reshape((-1,3))
+	# define criteria tuple and apply kmeans()
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+	ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# convert to numpy.float32
-Z = numpy.float32(Z)
+	# center now contains K RGB color values as a nested list
 
-# define how many colors (clusters) to use
-K = len(palette)
+	# reshape the labels from kmeans back to the shape of the resized_image image
+	labs = label.reshape((resized_image.shape[0], resized_image.shape[1]))
 
-# define criteria tuple and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+	# labs now contains a color classification integer for each pixel
 
-# center now contains K RGB color values as a nested list
+	pixel_ids = []
+	for row in labs:
+		pixel_ids.append([palette[lab] for lab in row])
 
-# reshape the labels from kmeans back to the shape of the resized image
-labs = label.reshape((resized.shape[0], resized.shape[1]))
+	pixel_ids = numpy.array(pixel_ids)
 
-# labs now contains a color classification integer for each pixel
+	# pixel_ids now contains the item ids from the palette for each pixel
 
-pixel_ids = []
-for row in labs:
-	pixel_ids.append([palette[lab] for lab in row])
+	# Now convert back into uint8, and make downsampled image
+	center = numpy.uint8(center)
+	res = center[label.flatten()]
+	res2 = res.reshape((resized_image.shape))
 
-pixel_ids = numpy.array(pixel_ids)
+	# for each pixel in the image, classify the color
+		# create an equal-dimension "image" with the classifications at each pixel
 
-# pixel_ids now contains the item ids from the palette for each pixel
+	# initialize the blueprint dictionary
+	bp = {"blueprint": {"tiles": [], "item": "blueprint"}}
 
-# Now convert back into uint8, and make downsampled image
-center = numpy.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((resized.shape))
+	# for each pixel in the classified "image", add a tile to the blueprint
+	# dictionary using the classification item ids with the associated position
+	for i, row in enumerate(pixel_ids):
+		for j, pixel in enumerate(row):
+			# construct the dictionary entry
+			entry = {"position": {"x": j, "y": i}, "name": pixel}
+			bp["blueprint"]["tiles"].append(entry)
+			# print(f'{e_counter}:({i},{j}): {pixel}')
 
-cv2.imwrite('output.jpg', res2)
+	# console.print_json(json.dumps(bp))
 
-# for each pixel in the image, classify the color
-	# create an equal-dimension "image" with the classifications at each pixel
+	# encode the json dict to a byte string
+	bp = str.encode(json.dumps(bp))
 
-newBp = {"blueprint": {"entities": [{"entity_number": 1, "name": "wooden-chest", "position": {"x": 1, "y": 1}}], "item": "blueprint"}}
+	# compress the byte string with zlib
+	bp = zlib.compress(bp, level=9)
 
-# initialize the blueprint dictionary
-bp = {"blueprint": {"tiles": [], "item": "blueprint"}}
+	# encode the compressed byte string to base64
+	bp = base64.b64encode(bp)
 
-e_counter = 1
-# for each pixel in the classified "image", add a tile to the blueprint
-# dictionary using the classification item ids with the associated position
-for i, row in enumerate(pixel_ids):
-	for j, pixel in enumerate(row):
-		# construct the dictionary entry
-		entry = {"position": {"x": j, "y": i}, "name": pixel}
-		bp["blueprint"]["tiles"].append(entry)
-		# print(f'{e_counter}:({i},{j}): {pixel}')
-		e_counter += 1
+	# push a zero version byte onto the front
+	bp = b'0' + bp
 
-# console.print_json(json.dumps(bp))
-
-# encode the json dict to a byte string
-bp = str.encode(json.dumps(bp))
-
-# compress the byte string with zlib
-bp = zlib.compress(bp, level=9)
-
-# encode the compressed byte string to base64
-bp = base64.b64encode(bp)
-
-# push a zero version byte onto the front
-bp = b'0' + bp
-
-# the blueprint string is formed
-# it is a byte string
-print(bp.decode('utf-8'))
+	# the blueprint string is formed!
+	# it is a byte string
+	return bp
---
+++ driver.py
@@ -0,0 +1,56 @@
+import os
+import cv2
+import bluepart as bp
+from flask import Flask, render_template, request, redirect, make_response, jsonify
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route('/')
+def welcome():
+	return render_template('index.html')
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def generate_blueprint():
+	if request.method == 'POST':
+		source_file = request.files['source']
+		print(source_file)
+
+		if source_file and allowed_file(source_file.filename):
+			filename = secure_filename(source_file.filename)
+			filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			source_file.save(filepath)
+
+		print(filepath)
+
+		scaling_factor = float(request.form.get("scale"))
+		print(scaling_factor)
+
+		tiles = request.form.getlist('tiles')
+		print(tiles)
+
+		# read the image
+		og_image = cv2.imread(filepath)
+
+		# delete the uploaded file for cleanliness
+		os.remove(filepath)
+
+		# rescale the input image
+		resized_image = bp.scale_image(og_image, scaling_factor)
+
+		# produce the blueprint string (as bytes)
+		bp_bytes = bp.construct_blueprint(resized_image, tiles)
+
+		message = bp_bytes.decode('utf-8')
+		response = make_response(message, 200)
+		response.mimetype = "text/plain"
+
+	return response
