528852b206cf82c89cd0bc50cb9b288d72801eba
mlgarrett <michael.garrett@nau.edu>
2023-04-30 00:43:53 -0700

    fixed forgotten server-side image scaling

--- bluepart.py
+++ bluepart.py
@@ -12,14 +12,15 @@ def scale_image(source, scale_percent):
 	# direction
 	if (source.shape[1]*source.shape[0] > 256**2):
 		scale = min(1, min(256/source.shape[1], 256/source.shape[0]))
-		bp_width = int(source.shape[1]*scale)
-		bp_height = int(source.shape[0]*scale)
+		bp_width = int(scale_percent*source.shape[1]*scale)
+		bp_height = int(scale_percent*source.shape[0]*scale)
 	else:
 		bp_width = int(source.shape[1] * scale_percent)
 		bp_height = int(source.shape[0] * scale_percent)
 
 	# scale the image
 	dim = (bp_width, bp_height)
+	print(dim)
 	  
 	# resize image
 	resized = cv2.resize(source, dim, interpolation = cv2.INTER_AREA)
